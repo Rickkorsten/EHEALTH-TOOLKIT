409a0da6e7fcdfcea4bce54e37108d7be7c9de27
Gielen,Tom T.J.M <t.gielen@student.fontys.nl>
2017-06-12 11:39:28 +0200

    Nieuw DESIGNNN

--- sample/src/main/java/link/fls/swipestacksample/MenuActivity.java
+++ sample/src/main/java/link/fls/swipestacksample/MenuActivity.java
@@ -21,8 +21,17 @@ public class MenuActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_menu);
 
         TextView welkomTitle = (TextView) findViewById(R.id.welkomTitle);
+        TextView random = (TextView) findViewById(R.id.buttonPerCategorie);
+        TextView overzicht = (TextView) findViewById(R.id.buttonOverzicht);
+        TextView geschiedenis = (TextView) findViewById(R.id.buttonGeschiedenis);
+        TextView eigen = (TextView) findViewById(R.id.buttonEigenKaart);
         Typeface comfortaa = Typeface.createFromAsset(getAssets(), "fonts/Comfortaa-Regular.ttf");
+        Typeface comfortaaBold = Typeface.createFromAsset(getAssets(), "fonts/Comfortaa-Bold.ttf");
         welkomTitle.setTypeface(comfortaa);
+        random.setTypeface(comfortaaBold);
+        overzicht.setTypeface(comfortaaBold);
+        geschiedenis.setTypeface(comfortaaBold);
+        eigen.setTypeface(comfortaaBold);
 
         goToKaartenOverzicht();
         goToCatagorieOverzicht();
